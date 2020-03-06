c05660b3e3742f6460ae04e2eab2c36c32427c6f
yigewu <yigewu@wustl.edu>
2020-03-06 16:20:08 -0600

    after run DEG for PT vs others

---
+++ integration/30_aliquot_integration/deg/findmarkers_pt_vs_others_wilcox.R
@@ -0,0 +1,77 @@
+#!/usr/bin/env Rscript
+
+## library
+packages = c(
+  "ggplot2",
+  "Seurat",
+  "dplyr",
+  "plyr",
+  "data.table"
+)
+
+for (pkg_name_tmp in packages) {
+  if (!(pkg_name_tmp %in% installed.packages()[,1])) {
+    print(paste0("No ", pkg_name_tmp, " Installed!"))
+  } else {
+    print(paste0("", pkg_name_tmp, " Installed!"))
+  }
+  library(package = pkg_name_tmp, character.only = T, quietly = T)
+}
+cat("Finish loading libraries!\n")
+cat("###########################################\n")
+
+## get the path to the seurat object
+args = commandArgs(trailingOnly=TRUE)
+
+## argument: directory to the output
+path_output_dir <- args[1]
+cat(paste0("Path to the output directory: ", path_output_dir, "\n"))
+cat("###########################################\n")
+
+## argument 2: filename for the output file
+path_output_filename <- args[2]
+cat(paste0("Filename for the output: ", path_output_filename, "\n"))
+cat("###########################################\n")
+path_output <- paste0(path_output_dir, path_output_filename)
+
+## argument : path to seurat object
+path_srat <- args[3]
+cat(paste0("Path to the seurat object: ", path_srat, "\n"))
+cat("###########################################\n")
+
+## argument : path to the barcode to cell type table
+path_barcode2celltype_df <- args[4]
+cat(paste0("Path to the barcode to cell type marker table: ", path_barcode2celltype_df, "\n"))
+cat("###########################################\n")
+
+## input cell type marker table
+barcode2celltype_df <- fread(input = path_barcode2celltype_df, data.table = F)
+cat("finish reading the barcode-cell-type table!\n")
+cat("###########################################\n")
+
+## input srat
+cat(paste0("Start reading the seurat object: ", "\n"))
+srat <- readRDS(path_srat)
+print("Finish reading the seurat object!\n")
+cat("###########################################\n")
+
+## add cell type info into meta data
+  srat@meta.data$barcode <- rownames(srat@meta.data)
+  metadata_tmp <- srat@meta.data
+  nrow(metadata_tmp)
+  metadata_tmp <- merge(metadata_tmp, barcode2celltype_df, by = c("barcode"), all.x = T)
+  nrow(metadata_tmp)
+  srat@meta.data <- metadata_tmp
+
+## change identification for the cells to be cell type group
+Idents(srat) <- "Cell_Type1"
+
+## run findallmarkers
+markers_df <- FindMarkers(object = srat, ident.1 = "Proximal tubule", logfc.threshold = 0.25, test.use = "wilcox", only.pos = T)
+print("Finish running FindMarkers!\n")
+cat("###########################################\n")
+
+## write output
+write.table(markers_df, file = path_output, quote = F, sep = "\t", row.names = F)
+cat("Finished saving the output\n")
+cat("###########################################\n")
