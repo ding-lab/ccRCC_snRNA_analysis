d66df5b255d3bccc7109d131e04f59fda413c7aa
yigewu <yigewu@wustl.edu>
2020-03-10 09:16:55 -0500

    finished average expression for tumor cells

--- integration/30_aliquot_integration/averageexpression/averageexpression_tumor_cells.R
+++ integration/30_aliquot_integration/averageexpression/averageexpression_tumor_cells.R
@@ -39,39 +39,21 @@ path_srat <- args[3]
 cat(paste0("Path to the seurat object: ", path_srat, "\n"))
 cat("###########################################\n")
 
-## argument : path to the barcode to cell type table
-path_barcode2celltype_df <- args[4]
-cat(paste0("Path to the barcode to cell type marker table: ", path_barcode2celltype_df, "\n"))
-cat("###########################################\n")
-
-## input cell type marker table
-barcode2celltype_df <- fread(input = path_barcode2celltype_df, data.table = F)
-cat("finish reading the barcode-cell-type table!\n")
-cat("###########################################\n")
-
 ## input srat
 cat(paste0("Start reading the seurat object: ", "\n"))
 srat <- readRDS(path_srat)
 print("Finish reading the seurat object!\n")
 cat("###########################################\n")
 
-## add cell type info into meta data
-metadata_tmp <- srat@meta.data
-metadata_tmp$integrated_barcode <- rownames(srat@meta.data)
-metadata_tmp <- merge(metadata_tmp, barcode2celltype_df, by = c("integrated_barcode"), all.x = T)
-rownames(metadata_tmp) <- metadata_tmp$integrated_barcode
-srat@meta.data <- metadata_tmp
+## change identification for the cells to be aliquot id
+Idents(srat) <- "orig.ident"
 
-## change identification for the cells to be cell type group
-Idents(srat) <- "Most_Enriched_Cell_Type1"
-
-## run findallmarkers
-markers_df <- FindMarkers(object = srat, ident.1 = "Proximal tubule", ident.2 = c("Endothelial cells", "Fibroblasts", "Loop of Henle", "Lymphoid lineage immune cells", "Myeloid lineage immune cells"), test.use = "wilcox", only.pos = T, logfc.threshold = 0)
-print("Finish running FindMarkers!\n")
-markers_df$gene <- rownames(markers_df)
+## run average expression
+aliquot.averages <- AverageExpression(srat)
+print("Finish running AverageExpression!\n")
 cat("###########################################\n")
 
 ## write output
-write.table(markers_df, file = path_output, quote = F, sep = "\t", row.names = F)
+write.table(aliquot.averages, file = path_output, quote = F, sep = "\t", row.names = T)
 cat("Finished saving the output\n")
 cat("###########################################\n")
