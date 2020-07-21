0ecf7a8a9ea0a2facaed2b96b225538da7bf9c4a
Yige Wu <yigewu@MacBook-Pro-4.local>
2020-07-21 08:31:06 -0500

    cell type marker dotplot v9

--- integration/30_aliquot_integration/dotplot/dotplot_celltypemarkers_in_epithelial_cells.R
+++ integration/30_aliquot_integration/dotplot/dotplot_celltypemarkers_in_epithelial_cells.R
@@ -24,7 +24,7 @@ source("./ccRCC_snRNA_analysis/functions.R")
 source("./ccRCC_snRNA_analysis/variables.R")
 library(ggplot2)
 ## set run id
-version_tmp <- 3
+version_tmp <- 2
 run_id <- paste0(format(Sys.Date(), "%Y%m%d") , ".v", version_tmp)
 ## set output directory
 dir_out <- paste0(makeOutDir_katmai(path_this_script), run_id, "/")
@@ -42,7 +42,7 @@ table(barcode2celltype_df$Cell_type.shorter)
 ## input DEG for each cell type
 gene2celltype_df <- fread(data.table = F, input = "./Resources/Knowledge/Kidney_Markers/Gene2CellType_Tab.20200406.v1.tsv")
 ## specify minimal percentage of cell in any clustering expressing the genes to show
-min.exp.pct <- 10
+min.exp.pct <- 25
 
 # modify srat object ------------------------------------------------------
 ## get barcodes to process
@@ -67,10 +67,10 @@ plot_data <- p$data
 ## transform the dataframe to matrix to better filter out genes with too low expressin
 plot_matrix <- dcast(data = plot_data, formula = features.plot ~ id, value.var = "pct.exp")
 plot_matrix %>% head()
-## filter for genes that are expressed in > min.exp.pct % of one cluster at least
 ## replot with the filtered genes plus malignant cell marker genes
-malignant_markers <- as.vector(gene2celltype_df$Gene[gene2celltype_df$Cell_Type_Group == "Malignant_Nephron_Epithelium"])
+### filter for genes that are expressed in > min.exp.pct % of one cluster at least
 genes2plot_filtered <- as.vector(plot_matrix[rowSums(plot_matrix[,unique(as.vector(plot_data$id))] > min.exp.pct) >= 1, "features.plot"])
+malignant_markers <- as.vector(gene2celltype_df$Gene[gene2celltype_df$Cell_Type_Group == "Malignant_Nephron_Epithelium"])
 genes2plot_filtered <- c(genes2plot_filtered, 
                          as.vector(plot_matrix[(plot_matrix$features.plot %in% malignant_markers) & (rowSums(plot_matrix[,unique(as.vector(plot_data$id))] > 5) >= 1), "features.plot"]))
 genes2plot_filtered <- unique(genes2plot_filtered)
