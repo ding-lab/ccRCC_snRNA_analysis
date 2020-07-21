7872b6ba467469e57bd11293f888bd84d8faf0b5
Yige Wu <yigewu@MacBook-Pro-4.local>
2020-07-21 08:48:26 -0500

    cell type marker dotplot v10

--- integration/30_aliquot_integration/dotplot/dotplot_celltypemarkers_in_epithelial_cells.R
+++ integration/30_aliquot_integration/dotplot/dotplot_celltypemarkers_in_epithelial_cells.R
@@ -24,7 +24,7 @@ source("./ccRCC_snRNA_analysis/functions.R")
 source("./ccRCC_snRNA_analysis/variables.R")
 library(ggplot2)
 ## set run id
-version_tmp <- 2
+version_tmp <- 3
 run_id <- paste0(format(Sys.Date(), "%Y%m%d") , ".v", version_tmp)
 ## set output directory
 dir_out <- paste0(makeOutDir_katmai(path_this_script), run_id, "/")
@@ -80,20 +80,24 @@ genes2plot_filtered <- unique(genes2plot_filtered)
 cat("###########################################\n")
 cat("Dotplot now\n")
 p <- DotPlot(object = srat, features = genes2plot_filtered, col.min = 0)
+p$data$id <- factor(x = p$data$id, levels = c("Tumor cells", "Proximal tubule", "Loop of Henle", "Distal convoluted tubule", "Intercalated cells", "Podocytes"))
 # p$data$gene_cell_type_group <- plyr::mapvalues(p$data$features.plot, from = gene2celltype_df$Gene, to = gene2celltype_df$Cell_Type_Group)
 # p$data$gene_cell_type1 <- plyr::mapvalues(p$data$features.plot, from = gene2celltype_df$Gene, to = gene2celltype_df$Cell_Type1)
 # p$data$gene_cell_type2 <- plyr::mapvalues(p$data$features.plot, from = gene2celltype_df$Gene, to = gene2celltype_df$Cell_Type2)
 # p$data$gene_cell_type3 <- plyr::mapvalues(p$data$features.plot, from = gene2celltype_df$Gene, to = gene2celltype_df$Cell_Type3)
 # p$data$gene_cell_type4 <- plyr::mapvalues(p$data$features.plot, from = gene2celltype_df$Gene, to = gene2celltype_df$Cell_Type4)
-p <- p  + RotatedAxis()
+# p <- p  + RotatedAxis()
 # p <- p + facet_grid(.~gene_cell_type1 + gene_cell_type2 + gene_cell_type3, scales = "free", space = "free", drop = T)
-p <- p + theme(panel.spacing = unit(0, "lines"),
-               strip.background = element_blank(),
-               panel.border = element_rect(colour = "black"),
-               panel.grid.major = element_line(colour = "grey50"),
-               strip.text.x = element_text(angle = 90, vjust = 0.5),
-               axis.text.x = element_text(angle = 90, face = "bold"),
-               strip.placement = "outside")
+# p <- p + theme(panel.spacing = unit(0, "lines"),
+#                strip.background = element_blank(),
+#                panel.border = element_rect(colour = "black"),
+#                panel.grid.major = element_line(colour = "grey50"),
+#                strip.text.x = element_text(angle = 90, vjust = 0.5),
+#                strip.placement = "outside")
+p <- p + theme(axis.text.x = element_text(angle = 90, face = "bold"),
+               axis.text.y = element_text(face = "bold"))
+p <- p + theme(axis.title.x = element_blank(),
+               axis.title.y = element_blank())
 cat("Finished Dotplot\n")
 cat("###########################################\n")
 
@@ -102,7 +106,5 @@ file2write <- paste0(dir_out, "Dotplot_CellTypeMarkers_Exp", ".pdf")
 pdf(file = file2write, width = 20, height = 6, useDingbats = F)
 print(p)
 dev.off()
-
 file2write <- paste0(dir_out, "Dotplot_CellTypeMarkers_Exp", ".RDS")
-saveRDS(object = file2write, compress = T)
-
+saveRDS(object = p, file = file2write, compress = T)
