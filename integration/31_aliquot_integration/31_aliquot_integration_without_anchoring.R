45ab983a02b0c29c7913d530a5048a2c9cadd98b
yigewu <yigewu@wustl.edu>
2020-07-28 07:21:46 -0500

    dotplot kidney epithelial cells with 31 samples v1

--- integration/31_aliquot_integration/31_aliquot_integration_without_anchoring.R
+++ integration/31_aliquot_integration/31_aliquot_integration_without_anchoring.R
@@ -46,7 +46,6 @@ length(renal.list)
 rm(seurat_obj)
 
 # Run the standard workflow for visualization and clustering ------------
-### normalization
 renal.list <- lapply(X = renal.list, FUN = function(x) {
   x <- NormalizeData(x)
   x <- FindVariableFeatures(x, selection.method = "vst", nfeatures = 3000)
@@ -56,10 +55,16 @@ renal.integrated <- merge(x = renal.list[[1]], y = renal.list[2:length(renal.lis
 rm(renal.list)
 ## scale data with all the features
 renal.integrated <- SCTransform(renal.integrated, vars.to.regress = c("nCount_RNA","percent.mito"), return.only.var.genes = F)
+cat("Finished SCTransform!\n")
 ## keep it consistant with individual processing pipeline
 renal.integrated <- RunPCA(renal.integrated, npcs = 30, verbose = FALSE)
+cat("Finished RUNPCA!\n")
 renal.integrated <- RunUMAP(renal.integrated, reduction = "pca", dims = 1:30)
+cat("Finished RUNUMAP!\n")
 renal.integrated <- FindNeighbors(renal.integrated, reduction = "pca", dims = 1:30, force.recalc = T)
+cat("Finished FindNeighbors!\n")
 renal.integrated <- FindClusters(renal.integrated, resolution = 0.5)
+cat("Finished FindClusters!\n")
 ## save as RDS file
 saveRDS(object = renal.integrated, file = paste0(dir_out, "31_aliquot_integration_without_anchoring.", run_id, ".RDS"), compress = T)
+cat("Finished saving the output!\n")
