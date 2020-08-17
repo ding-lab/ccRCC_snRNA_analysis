a89ed229ce513eeba4fbfa47880d449727e94610
Yige Wu <yigewu@macbook-pro-4.local.dhcp.wustl.edu>
2020-08-17 17:26:33 -0500

    findmarkers for C3L-00010 related samples v2

--- findmarkers/tumor_subclusters/findallmarkers_C3L-00010_correlated_tumorcells_bynewcluster_on_katmai.R
+++ findmarkers/tumor_subclusters/findallmarkers_C3L-00010_correlated_tumorcells_bynewcluster_on_katmai.R
@@ -1,8 +1,23 @@
 # Yige Wu @WashU Feb 2020
 
 # set up libraries and output directory -----------------------------------
+## getting the path to the current script
+thisFile <- function() {
+  cmdArgs <- commandArgs(trailingOnly = FALSE)
+  needle <- "--file="
+  match <- grep(needle, cmdArgs)
+  if (length(match) > 0) {
+    # Rscript
+    return(normalizePath(sub(needle, "", cmdArgs[match])))
+  } else {
+    # 'source'd via R console
+    return(normalizePath(sys.frames()[[1]]$ofile))
+  }
+}
+path_this_script <- thisFile()
 ## set working directory
-dir_base = "~/Box/Ding_Lab/Projects_Current/RCC/ccRCC_snRNA/"
+dir_base = "/diskmnt/Projects/ccRCC_scratch/ccRCC_snRNA/"
+# dir_base = "~/Box/Ding_Lab/Projects_Current/RCC/ccRCC_snRNA/"
 setwd(dir_base)
 source("./ccRCC_snRNA_analysis/load_pkgs.R")
 source("./ccRCC_snRNA_analysis/functions.R")
@@ -11,7 +26,7 @@ source("./ccRCC_snRNA_analysis/variables.R")
 version_tmp <- 1
 run_id <- paste0(format(Sys.Date(), "%Y%m%d") , ".v", version_tmp)
 ## set output directory
-dir_out <- paste0(makeOutDir(), run_id, "/")
+dir_out <- paste0(makeOutDir_katmai(path_this_script), run_id, "/")
 dir.create(dir_out)
 
 # input dependencies ------------------------------------------------------
