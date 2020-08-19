101d0d9d0595f14a826ca0796f02c1e3aa9d3c2e
Yige Wu <yigewu@macbook-pro-4.local.dhcp.wustl.edu>
2020-08-19 07:55:59 -0500

    violin C3L-00010 correlated samples v5

--- findmarkers/tumor_subclusters/plotting/violinplot_C3L-00010_correlated_tumorcells_tumormanualsubcluster_selected_degs_on_katmai.R
+++ findmarkers/tumor_subclusters/plotting/violinplot_C3L-00010_correlated_tumorcells_tumormanualsubcluster_selected_degs_on_katmai.R
@@ -23,6 +23,7 @@ setwd(dir_base)
 source("./ccRCC_snRNA_analysis/load_pkgs.R")
 source("./ccRCC_snRNA_analysis/functions.R")
 source("./ccRCC_snRNA_analysis/variables.R")
+library(ggplot2)
 ## set run id
 version_tmp <- 1
 run_id <- paste0(format(Sys.Date(), "%Y%m%d") , ".v", version_tmp)
