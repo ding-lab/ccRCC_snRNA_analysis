6e8f5045f2047298767f1a2419848b3e05755ef1
Yige Wu <yigewu@macbook-pro-4.local.dhcp.wustl.edu>
2020-12-15 10:02:44 -0600

    DEG for tumor cells bap1-tumor vs non-mutant v1

--- findmarkers/tumor_vs_normal/findallmarker_wilcox_atac_BAP1_tumor_vs_nonmutant_on_katmai.R
+++ findmarkers/tumor_vs_normal/findallmarker_wilcox_atac_BAP1_tumor_vs_nonmutant_on_katmai.R
@@ -25,7 +25,7 @@ source("./ccRCC_snRNA_analysis/load_pkgs.R")
 source("./ccRCC_snRNA_analysis/functions.R")
 source("./ccRCC_snRNA_analysis/variables.R")
 ## set run id
-version_tmp <- 2
+version_tmp <- 1
 run_id <- paste0(format(Sys.Date(), "%Y%m%d") , ".v", version_tmp)
 ## set output directory
 dir_out <- paste0(makeOutDir_katmai(path_this_script), run_id, "/")
@@ -51,18 +51,17 @@ assay_process <- "RNA"
 cat(paste0("Assay: ", assay_process, "\n"))
 cat("###########################################\n")
 ## specify cell groups to compare
-group1_findmarkers <- "Macrophages_Tumor"
-group2_findmarkers <- "Proximal tubule cells from NATs"
+group1_findmarkers <- "Tumor_cells_BAP1-mutant"
+group2_findmarkers <- "Tumor_cells_non-mutant"
 
 # subset to cases with snATAC data ----------------------------------------
-easyids_snatac <- c("C3N-00733-T1", "C3L-00610-T1", "C3L-01313-T1", "C3L-00416-T2", "C3L-01287-T1", "C3L-00917-T1", "C3L-00088-T1", "C3N-01200-T1", "C3L-00088-T2", "C3L-00079-T1", "C3L-00448-T1",
-                    "C3L-00088-N", "C3N-01200-N")
-aliquots_snatac <- idmetadata_df$Aliquot.snRNA[idmetadata_df$Aliquot.snRNA.WU %in% easyids_snatac]
-aliquots_snatac
-aliquots_snatac_nat <- idmetadata_df$Aliquot.snRNA[idmetadata_df$Aliquot.snRNA.WU %in% easyids_snatac & idmetadata_df$Sample_Type == "Normal"]
-aliquots_snatac_nat
-aliquots_snatac_tumor <- idmetadata_df$Aliquot.snRNA[idmetadata_df$Aliquot.snRNA.WU %in% easyids_snatac & idmetadata_df$Sample_Type == "Tumor"]
-aliquots_snatac_tumor
+easyids_snatac_group1 <- c("C3L-00416-T2", "C3L-01313-T1", "C3N-01200-T1")
+aliquots_snatac_group1 <- idmetadata_df$Aliquot.snRNA[idmetadata_df$Aliquot.snRNA.WU %in% easyids_snatac_group1 & idmetadata_df$Sample_Type == "Tumor"]
+
+easyids_snatac_group2 <- c("C3L-00088-T1", "C3L-00088-T2", "C3L-00917-T1", "C3L-00448-T1")
+aliquots_snatac_group2 <- idmetadata_df$Aliquot.snRNA[idmetadata_df$Aliquot.snRNA.WU %in% easyids_snatac_group2 & idmetadata_df$Sample_Type == "Tumor"]
+
+aliquots_snatac <- c(aliquots_snatac_group1, aliquots_snatac_group2)
 Idents(srat) <- "orig.ident"
 srat <- subset(srat, idents = aliquots_snatac)
 cat("finish subsetting the object!\n")
@@ -80,36 +79,39 @@ cat("finish adding unique id for each barcode in the seurat object!\n")
 
 # run findallmarkers by tumor------------------------------------------------------
 degs_combined_df <- NULL
-for (aliquot_tumor_tmp in aliquots_snatac_tumor) {
-  easyid_tumor_tmp <- idmetadata_df$Aliquot.snRNA.WU[idmetadata_df$Aliquot.snRNA == aliquot_tumor_tmp]
-  cat(paste0("Processing ", easyid_tumor_tmp, "\n"))
-  ## make combined id for the barcode2celltype table
-  barcode2celltype_df <- barcode2celltype_df %>%
-    mutate(id_aliquot_barcode = paste0(orig.ident, "_", individual_barcode)) %>%
-    mutate(group_findmarkers = ifelse(Cell_group5 == "Tumor cells" & orig.ident == aliquot_tumor_tmp, 
-                                      "Tumor cells", 
-                                      ifelse(Cell_type.detailed == "Proximal tubule" & orig.ident %in% aliquots_snatac_nat, "Proximal tubule cells from NATs", "Others")))
-  ## map group label
-  srat@meta.data$group_findmarkers <- mapvalues(x = srat@meta.data$id_aliquot_barcode, from = barcode2celltype_df$id_aliquot_barcode, to = as.vector(barcode2celltype_df$group_findmarkers), warn_missing = F)
-  cat("finish adding group labels\n")
-  
-  print(table(srat@meta.data$group_findmarkers))
-  Idents(srat) <- "group_findmarkers" 
-  ## run findmarkers
-  deg_df <- FindMarkers(object = srat, test.use = "wilcox", ident.1 = group1_findmarkers, ident.2 = group2_findmarkers, only.pos = F,
-                               min.pct = min.pct.run, logfc.threshold = logfc.threshold.run, min.diff.pct = min.diff.pct.run, verbose = T)
-  deg_df$genesymbol_deg <- rownames(deg_df)
-  deg_df$easyid_tumor <- easyid_tumor_tmp
-  deg_df$aliquot_tumor <- aliquot_tumor_tmp
-  deg_df$cellnumber_tumorcells <- length(which(srat@meta.data$group_findmarkers == group1_findmarkers))
-  deg_df$cellnumber_ptcells <- length(which(srat@meta.data$group_findmarkers == group2_findmarkers))
-  
-  ## combine with the super tale
-  degs_combined_df <- rbind(degs_combined_df, deg_df)
+for (aliquot_group1_tmp in aliquots_snatac_group1) {
+  easyid_group1_tmp <- idmetadata_df$Aliquot.snRNA.WU[idmetadata_df$Aliquot.snRNA == aliquot_group1_tmp]
+  for (aliquot_group2_tmp in aliquots_snatac_group2) {
+    easyid_group2_tmp <- idmetadata_df$Aliquot.snRNA.WU[idmetadata_df$Aliquot.snRNA == aliquot_group2_tmp]
+    cat(paste0("Processing ", easyid_group1_tmp, " and ", easyid_group2_tmp, "\n"))
+    ## make combined id for the barcode2celltype table
+    barcode2celltype_df <- barcode2celltype_df %>%
+      mutate(id_aliquot_barcode = paste0(orig.ident, "_", individual_barcode)) %>%
+      mutate(group_findmarkers = ifelse(Cell_group5 == "Tumor cells" & orig.ident == aliquot_group1_tmp, 
+                                        "Tumor_cells_BAP1-mutant", 
+                                        ifelse(Cell_group5 == "Tumor cells" & orig.ident == aliquot_group2_tmp, "Tumor_cells_non-mutant", "Others")))
+    ## map group label
+    srat@meta.data$group_findmarkers <- mapvalues(x = srat@meta.data$id_aliquot_barcode, from = barcode2celltype_df$id_aliquot_barcode, to = as.vector(barcode2celltype_df$group_findmarkers), warn_missing = F)
+    cat("finish adding group labels\n")
+    
+    print(table(srat@meta.data$group_findmarkers))
+    Idents(srat) <- "group_findmarkers" 
+    ## run findmarkers
+    deg_df <- FindMarkers(object = srat, test.use = "wilcox", ident.1 = group1_findmarkers, ident.2 = group2_findmarkers, only.pos = F,
+                          min.pct = min.pct.run, logfc.threshold = logfc.threshold.run, min.diff.pct = min.diff.pct.run, verbose = T)
+    deg_df$genesymbol_deg <- rownames(deg_df)
+    deg_df$easyid_group1 <- easyid_group1_tmp
+    deg_df$easyid_group2 <- easyid_group2_tmp
+    deg_df$cellnumber_group1 <- length(which(srat@meta.data$group_findmarkers == group1_findmarkers))
+    deg_df$cellnumber_group2 <- length(which(srat@meta.data$group_findmarkers == group2_findmarkers))
+    
+    ## combine with the super tale
+    degs_combined_df <- rbind(degs_combined_df, deg_df)
+  }
 }
 
 # write output ------------------------------------------------------------
-file2write <- paste0(dir_out, "findallmarkers_wilcox_each_snatac_tumor_vs_pt.", run_id, ".tsv")
+file2write <- paste0(dir_out, "findallmarkers_wilcox_each_snatac_bap1_tumor_vs_nonmutant.tumorcells.", run_id, ".tsv")
 write.table(x = degs_combined_df, file = file2write, sep = "\t", quote = F, row.names = F)
 cat("finish writing the result!\n")
 
