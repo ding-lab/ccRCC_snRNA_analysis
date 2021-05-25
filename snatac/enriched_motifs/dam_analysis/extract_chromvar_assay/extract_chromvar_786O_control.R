127c32518b396db0a7ec4106cde1f1874950f0b9
Yige Wu <yigewu@macbook-pro-4.local.dhcp.wustl.edu>
2021-05-25 17:07:08 -0500

    extract chromvar v1

---
+++ snatac/enriched_motifs/dam_analysis/extract_chromvar_assay/extract_chromvar_786O_control.R
@@ -0,0 +1,43 @@
+# Yige Wu @WashU May 2021
+
+# set up libraries and output directory -----------------------------------
+## getting the path to the current script
+thisFile <- function() {
+  cmdArgs <- commandArgs(trailingOnly = FALSE)
+  needle <- "--file="
+  match <- grep(needle, cmdArgs)
+  if (length(match) > 0) {
+    # Rscript
+    return(normalizePath(sub(needle, "", cmdArgs[match])))
+  } else {
+    # 'source'd via R console
+    return(normalizePath(sys.frames()[[1]]$ofile))
+  }
+}
+path_this_script <- thisFile()
+## set working directory
+dir_base = "/diskmnt/Projects/ccRCC_scratch/ccRCC_snRNA/"
+# dir_base = "~/Box/Ding_Lab/Projects_Current/RCC/ccRCC_snRNA/"
+setwd(dir_base)
+library(Signac)
+library(Seurat)
+source("./ccRCC_snRNA_analysis/load_pkgs.R")
+source("./ccRCC_snRNA_analysis/functions.R")
+## set run id
+version_tmp <- 1
+run_id <- paste0(format(Sys.Date(), "%Y%m%d") , ".v", version_tmp)
+## set output directory
+dir_out <- paste0(makeOutDir_katmai(path_this_script), run_id, "/")
+dir.create(dir_out)
+
+# input dependencies ------------------------------------------------------
+## input the ATAC object
+atac_obj <- readRDS(file = "/diskmnt/Projects/ccRCC_scratch/ccRCC_snATAC/Resources/snATAC_Processed_Data/Signac.1.0.0/4.Cell_lines/control-20210412-786-O_processed_atac.chromvar.rds")
+
+# extract -----------------------------------------------------------------
+DefaultAssay(atac_obj) <- 'chromvar'
+chromv= GetAssayData(object = atac_obj)
+
+# write output ------------------------------------------------------------
+file2write <- paste0(dir_out, "786O_control.chromvar.tsv")
+write.table(x = chromv, file = file2write, quote = F, row.names = T, sep = "\t")
