4eb1f69a3e123dcead79cd471b047aa795996a7d
Yige Wu <yigewu@macbook-pro-4.local.dhcp.wustl.edu>
2021-05-27 16:31:10 -0500

    correlate expression methylation v1

--- bulk/methylation/correlate_methyl_probe_to_rna_parallel_katmai.R
+++ bulk/methylation/correlate_methyl_probe_to_rna_parallel_katmai.R
@@ -1,15 +1,33 @@
+# Yige Wu @WashU May 2021
+## source activate ccrcc_snrna
+
 # set up libraries and output directory -----------------------------------
+## getting the path to the current script
+thisFile <- function() {
+  cmdArgs <- commandArgs(trailingOnly = FALSE)
+  needle <- "--file="
+  match <- grep(needle, cmdArgs)
+  if (length(match) > 0) {
+    # Rscript
+    return(normalizePath(sub(needle, "", cmdArgs[match])))
+  } else {
+    # 'source'd via R console
+    return(normalizePath(sys.frames()[[1]]$ofile))
+  }
+}
+path_this_script <- thisFile()
 ## set working directory
-dir_base = "~/Box/Ding_Lab/Projects_Current/RCC/ccRCC_snRNA/"
+dir_base = "/diskmnt/Projects/ccRCC_scratch/ccRCC_snRNA/"
+# dir_base = "~/Box/Ding_Lab/Projects_Current/RCC/ccRCC_snRNA/"
 setwd(dir_base)
+## library additional libaries
 source("./ccRCC_snRNA_analysis/load_pkgs.R")
 source("./ccRCC_snRNA_analysis/functions.R")
-source("./ccRCC_snRNA_analysis/variables.R")
 ## set run id
 version_tmp <- 1
 run_id <- paste0(format(Sys.Date(), "%Y%m%d") , ".v", version_tmp)
 ## set output directory
-dir_out <- paste0(makeOutDir(), run_id, "/")
+dir_out <- paste0(makeOutDir_katmai(path_this_script), run_id, "/")
 dir.create(dir_out)
 
 # input dependencies ------------------------------------------------------
