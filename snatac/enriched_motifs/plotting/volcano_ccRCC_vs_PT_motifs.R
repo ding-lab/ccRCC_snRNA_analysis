9438ec8b109c59a5fe3677e8d909fd3b07e37344
Yige Wu <yigewu@macbook-pro-4.local.dhcp.wustl.edu>
2021-06-03 11:55:27 -0500

    coverage plot da peak v1

--- snatac/enriched_motifs/plotting/volcano_ccRCC_vs_PT_motifs.R
+++ snatac/enriched_motifs/plotting/volcano_ccRCC_vs_PT_motifs.R
@@ -16,17 +16,7 @@ dir_out <- paste0(makeOutDir(), run_id, "/")
 dir.create(dir_out)
 
 # input dependencies ------------------------------------------------------
-dam_df <- fread(data.table = F, input = "./Resources/snATAC_Processed_Data/Enriched_Motifs/")
-## 
-da_bycelltype_df <- fread(data.table = F, input = "../ccRCC_snATAC/Resources/snATAC_Processed_Data/Enriched_Motifs/TransitionalPopulation/C3L_00079_T1_Score_difference.20201111.tsv")
-
-# filter the emt genes ----------------------------------------------------
-da_bycelltype_filtered_df <- da_bycelltype_df %>%
-  filter(p_val_adj < 0.05) %>%
-  arrange(desc(diff))
-da_bycelltype_filtered_df <- da_bycelltype_filtered_df[!duplicated(da_bycelltype_filtered_df$TF),]
-tfs_emt_tumorcells <- da_bycelltype_filtered_df$TF[da_bycelltype_filtered_df$cell_t2 == "Transitional cells"]
-tfs_tumorcells <- da_bycelltype_filtered_df$TF[da_bycelltype_filtered_df$cell_t2 == "Tumor cells"]
+dam_df <- fread(data.table = F, input = "./Resources/snATAC_Processed_Data/Enriched_Motifs/Score_difference.Tumor_Normal_comparison.20210509.tsv")
 
 # set plotting parameters -------------------------------------------------
 ## set y bottom threshold
@@ -35,52 +25,60 @@ y_bottom <- -log10(0.05)
 x_pos <- 0.5
 x_neg <- -0.5
 ## colors
-color_right_deep <- RColorBrewer::brewer.pal(n = 12, name = "Paired")[12]
-color_right_pale <- RColorBrewer::brewer.pal(n = 7, name = "Set2")[7]
-color_left_deep <- RColorBrewer::brewer.pal(n = 6, name = "Dark2")[4]
-color_left_pale <- RColorBrewer::brewer.pal(n = 6, name = "Set2")[4]
+color_purple <- RColorBrewer::brewer.pal(n = 4, name = "Set1")[4]
+color_red <- RColorBrewer::brewer.pal(n = 4, name = "Set1")[1]
+color_blue <- RColorBrewer::brewer.pal(n = 4, name = "Set1")[2]
 
 # make data for plotting --------------------------------------------------
-plot_data_df <- da_df %>%
-  mutate(Is_FOSJUN = grepl(pattern = "FOS|JUN", x = TF)) %>%
-  mutate(Log10p_val_adj = -log10(x = p_val_adj)) %>%
-  mutate(x_plot = diff) %>%
-  arrange(desc(diff))
-## cap y axis
-y_cap <- max(plot_data_df$Log10p_val_adj[!is.infinite(plot_data_df$Log10p_val_adj)])
+dam_sig_df <- dam_df %>%
+  mutate(log10_pvalue = -log10(pvalue)) %>%
+  mutate(log10_pvalue_capped = ifelse(is.infinite(log10_pvalue), 150, log10_pvalue)) %>%
+  filter(FDR < 0.05)
+
+plot_data_df <- dam_sig_df %>%
+  group_by(TF_Name) %>%
+  summarise(Num_sig_up = length(which(diff > 0)), Num_sig_down = length(which(diff < 0)),
+            avg_log10_pvalue = mean(log10_pvalue_capped), avg_diff = mean(diff)) %>%
+  mutate(foldchange_type = ifelse(Num_sig_down == 0, "consistently higher in ccRCC",
+                                  ifelse(Num_sig_up == 0, "consistently lower in ccRCC", "Mixed fold change directions"))) %>%
+  # mutate(number_foldchange = ifelse(avg_diff > 0, Num_sig_up, Num_sig_down)) %>%
+  mutate(size_plot = abs(Num_sig_up - Num_sig_down)/24) %>%
+  mutate(x_plot = ifelse(avg_diff < -2, -2, ifelse(avg_diff > 2, 2,  avg_diff))) %>%
+  mutate(y_plot = avg_log10_pvalue) %>%
+  arrange(desc(x_plot))
 ## decide TFs to show
-plot_data_filtered_df <- plot_data_df %>%
-  filter(TF %in% c(tfs_emt_tumorcells, tfs_tumorcells))
-tfs_right <- plot_data_filtered_df$TF[!(plot_data_filtered_df$Is_FOSJUN) & plot_data_filtered_df$diff > 0 & plot_data_filtered_df$mean_score1 > 0 & plot_data_filtered_df$TF %in% tfs_emt_tumorcells] %>%
-  head(19)
-tfs_right <- c(tfs_right, "FOS::JUN")
-tfs_left <- plot_data_filtered_df$TF[!(plot_data_filtered_df$Is_FOSJUN) & plot_data_filtered_df$diff < 0 & plot_data_filtered_df$mean_score2 > 0 & plot_data_filtered_df$TF %in% tfs_tumorcells] %>%
-  tail(20)
-tfs_left
+tfnames_show <- plot_data_df$TF_Name[plot_data_df$size_plot == 1]
 ## label TFS to show
 plot_data_df <- plot_data_df %>%
-  mutate(y_plot = ifelse(Log10p_val_adj >= y_cap, y_cap, Log10p_val_adj)) %>%
-  mutate(TF_modified = gsub(x = TF, pattern = "\\(var.2\\)", replacement = "*")) %>%
-  mutate(text_TF = ifelse(TF %in% c(tfs_right, tfs_left), TF_modified, NA))
+  # mutate(TF_modified = gsub(x = TF_Name, pattern = "\\(var.2\\)", replacement = "*")) %>%
+  mutate(text_tf = ifelse(TF_Name %in% tfnames_show, TF_Name, NA))
 
 # plot all markers--------------------------------------------------------------------
 ## plot
 p <- ggplot()
 p <- p + geom_vline(xintercept = 0, linetype = 2, color = "grey70")
-p <- p + geom_point(data = subset(plot_data_df, y_plot < y_bottom), mapping = aes(x = x_plot, y = y_plot), alpha = 0.5, size = 0.5, color = "grey70")
-p <- p + geom_point(data = subset(plot_data_df, y_plot >= y_bottom & x_plot > 0), mapping = aes(x = x_plot, y = y_plot), alpha = 0.5, size = 0.5, color = color_right_deep)
-p <- p + geom_point(data = subset(plot_data_df, y_plot >= y_bottom & x_plot < 0), mapping = aes(x = x_plot, y = y_plot), alpha = 0.5, size = 0.5, color = color_left_deep)
+# p <- p + geom_point(data = subset(plot_data_df), mapping = aes(x = x_plot, y = y_plot, size = size_plot, color = foldchange_type), alpha = 0.5)
+p <- p + geom_point(data = subset(plot_data_df, foldchange_type == "Mixed fold change directions"), mapping = aes(x = x_plot, y = y_plot, size = size_plot), alpha = 0.5, color = color_purple)
+p <- p + geom_point(data = subset(plot_data_df, foldchange_type == "consistently higher in ccRCC"), mapping = aes(x = x_plot, y = y_plot, size = size_plot), alpha = 0.8, color = color_red)
+p <- p + geom_point(data = subset(plot_data_df, foldchange_type == "consistently lower in ccRCC"), mapping = aes(x = x_plot, y = y_plot, size = size_plot), alpha = 0.8, color = color_blue)
 # p <- p + scale_color_manual(values = c("FDR<0.05 (up)" = "red", "FDR<0.05 (down)" = "blue", "FDR<0.05" = "black", "FDR>=0.05" = "grey80"))
 # p <- p + geom_text_repel(data = subset(plot_data_df, !is.na(text_TF)),
 #                          mapping = aes(x = x_plot, y = y_plot, label = text_TF), color = "black", force = 4, fontface = "bold", segment.alpha = 0.5)
-p <- p + geom_text_repel(data = subset(plot_data_df, !is.na(text_TF) & x_plot > 0),
-                         mapping = aes(x = x_plot, y = y_plot, label = text_TF), color = "black", force = 4, alpha = 0.8, size = 6, segment.size = 0.2, segment.alpha = 0.5)
-p <- p + geom_text_repel(data = subset(plot_data_df, !is.na(text_TF) & x_plot < 0),
-                         mapping = aes(x = x_plot, y = y_plot, label = text_TF), color = "black", force = 3, alpha = 0.8, segment.size = 0.2, segment.alpha = 0.5)
-p <- p + xlim(-3, NA)
+p <- p + geom_text_repel(data = subset(plot_data_df, !is.na(text_tf) & x_plot > 0),
+                         mapping = aes(x = x_plot, y = y_plot, label = text_tf), 
+                         color = "black", alpha = 1, size = 3, 
+                         segment.size = 0.4, segment.alpha = 1, min.segment.length = 0,
+                         xlim = c(0, NA))
+p <- p + geom_text_repel(data = subset(plot_data_df, !is.na(text_tf) & x_plot < 0),
+                         mapping = aes(x = x_plot, y = y_plot, label = text_tf), 
+                         color = "black", alpha = 1, size = 3, 
+                         segment.size = 0.4, segment.alpha = 1, min.segment.length = 0,
+                         xlim = c(NA, 0))
+p <- p + scale_size_area(max_size = 4)
 p <- p + theme_classic()
-p <- p + xlab("Bias-corrected accessibility deviations")
-p <- p + ylab("-Log10(P-value-adjusted)")
+p <- p + xlab("Bias-corrected accessibility deviations (average)")
+p <- p + ylab("-Log10FDR (average)")
+# p <- p + guides(fill=guide_legend(title="New Legend Title"))
 p <- p + theme(axis.text = element_text(size = 14),
                axis.title = element_text(size = 15))
 p
@@ -88,8 +86,7 @@ file2write <- paste0(dir_out, "volcano.", "png")
 png(file2write, width = 800, height = 600, res = 150)
 print(p)
 dev.off()
-
-file2write <- paste0(dir_out, "volcano.", "pdf")
-pdf(file2write, width = 6, height = 5, useDingbats = F)
-print(p)
-dev.off()
+# file2write <- paste0(dir_out, "volcano.", "pdf")
+# pdf(file2write, width = 6, height = 5, useDingbats = F)
+# print(p)
+# dev.off()
