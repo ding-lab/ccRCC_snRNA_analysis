46c2ec36e5a0de951f7f44aae915dcee086e8fea
Yige Wu <yigewu@macbook-pro-4.local.dhcp.wustl.edu>
2021-06-09 12:31:08 -0500

    merge 33 aliquot with 2 PKD normals v1

--- merging/merge_33_aliquot_w_2PKDnormals.R
+++ merging/merge_33_aliquot_w_2PKDnormals.R
@@ -29,7 +29,9 @@ run_id <- paste0(format(Sys.Date(), "%Y%m%d") , ".v", version_tmp)
 ## set output directory
 dir_out <- paste0(makeOutDir_katmai(path_this_script), run_id, "/")
 dir.create(dir_out)
-options(future.globals.maxSize = 1000 * 1024^2)
+library(future)
+plan("multiprocess", workers = 5)
+options(future.globals.maxSize = 7 * 1024^3) # for 7 Gb RAM
 
 # input dependencies ------------------------------------------------------
 ## input seurat paths
@@ -37,29 +39,43 @@ paths_srat <- fread(data.table = F, input = "./Data_Freezes/V2/snRNA/Seurat_Obje
 ## input doublet prediction
 barcode2scrublet_df <- fread(input = "./Resources/Analysis_Results/doublet/unite_scrublet_outputs/20210428.v1/scrublet.united_outputs.20210428.v1.tsv", data.table = F)
 
-## scale data with all the features
-path_final_file <- paste0(dir_out, "33_aliquot_merged_w_2PKDnormals.", run_id, ".RDS")
-path_sct_file <- paste0(dir_out, "SCT." , ".RDS")
+# make united paths -------------------------------------------------------
+paths_srat2process <- paths_srat %>%
+  select(Aliquot, Path_katmai_seurat_object)
+paths_srat2process <- rbind(data.frame(Aliquot = c("HTHK1900070", 
+                                                   "HTHK190043"),
+                                       Path_katmai_seurat_object = c("/diskmnt/Projects/Users/rliu/Projects/PKD/Seurat/HTHK1900070/nfeature_400_3000_ncount_1000_7000_mito_0.1/HTHK1900070_processed.rds",
+                                                                     "/diskmnt/Projects/Users/rliu/Projects/PKD/Seurat/HTHK190043/nfeature_400_6000_ncount_1000_Inf_mito_0.1/HTHK190043_processed.rds")), 
+                            paths_srat2process)
+
+# process -----------------------------------------------------------------
+path_final_file <- paste0(dir_out, "33_aliquot_w_2PKDnormals.Merged.", run_id, ".RDS")
+path_sct_file <- paste0(dir_out, "33_aliquot_w_2PKDnormals.SCT.", run_id , ".RDS")
 
 if (!file.exists(path_sct_file)) {
   # input the seurat object and store in a list--------------------------------------------------------
-  paths_srat2process <- paths_srat
   renal.list <- list()
   for (i in 1:nrow(paths_srat2process)) {
     sample_id_tmp <- paths_srat2process$Aliquot[i]
     seurat_obj_path <- paths_srat2process$Path_katmai_seurat_object[i]
     seurat_obj <- readRDS(file = seurat_obj_path)
-    ## take out the doublets
-    barcode2scrublet_tmp_df <- barcode2scrublet_df %>%
-      filter(Aliquot == sample_id_tmp) %>%
-      filter(Barcode %in% rownames(seurat_obj@meta.data)) %>%
-      filter(!predicted_doublet)
-    barcodes_keep <- barcode2scrublet_tmp_df$Barcode
-    ###
-    print("subsetting")
-    seurat_sub_obj <- subset(x = seurat_obj, cells = barcodes_keep)
-    print(dim(seurat_sub_obj))
-    renal.list[[sample_id_tmp]] <- seurat_sub_obj
+    if (sample_id_tmp %in% barcode2scrublet_df$Aliquot) {
+      ## take out the doublets
+      barcode2scrublet_tmp_df <- barcode2scrublet_df %>%
+        filter(Aliquot == sample_id_tmp) %>%
+        filter(Barcode %in% rownames(seurat_obj@meta.data)) %>%
+        filter(!predicted_doublet)
+      barcodes_keep <- barcode2scrublet_tmp_df$Barcode
+      ### subsetting
+      print("subsetting")
+      seurat_sub_obj <- subset(x = seurat_obj, cells = barcodes_keep)
+      print(dim(seurat_sub_obj))
+      renal.list[[sample_id_tmp]] <- seurat_sub_obj
+    } else {
+      print(dim(seurat_obj))
+      renal.list[[sample_id_tmp]] <- seurat_obj
+    }
+
   }
   length(renal.list)
   rm(seurat_obj)
