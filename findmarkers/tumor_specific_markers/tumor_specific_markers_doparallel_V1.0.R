265e01325f52fbcd0c1dace6c1e570780f5f3791
Yige Wu <yigewu@macbook-pro-4.local.dhcp.wustl.edu>
2021-07-01 12:05:45 -0500

    tumor-specific markers v1

--- findmarkers/tumor_specific_markers/tumor_specific_markers_doparallel_V1.0.R
+++ findmarkers/tumor_specific_markers/tumor_specific_markers_doparallel_V1.0.R
@@ -148,8 +148,10 @@ tumor_vs_rest_DE_total_list<-foreach(sample_id=samples) %dopar% {
   }
   
   #step1 tumore vs all the rest cell types
-  cat("STEP1 DEG analysis between tumor cells and other cell populations as as whole...\n")
+  cat(paste0("STEP1 DEG analysis between tumor cells and other cell populations as as whole for sample", sample_id, "...\n"))
   DE_genes_tmp <- tumor_vs_rest_DE_fun(sobj=sobj,tumor_ct=tumor_ct,sample = sample_id)
+  cat(paste0("Finished STEP1 for sample", sample_id, "...\n"))
+  print(head(DE_genes_tmp))
   #append avg exp of tumor cells to the DE_genes_tmp df
   row_genes <- rownames(DE_genes_tmp) %>% strsplit("[.]") %>% lapply("[",2) %>% unlist 
   DefaultAssay(sobj)<-"RNA"
@@ -160,6 +162,8 @@ tumor_vs_rest_DE_total_list<-foreach(sample_id=samples) %dopar% {
   norm_exp_avg$gene_symbol <- rownames(norm_exp_avg)
   
   DE_genes_tmp <- merge(DE_genes_tmp,norm_exp_avg,by="gene_symbol",all.x=TRUE,incomparables = NA,sort=FALSE)
+  cat(paste0("Finished adding avg_norm_exp for sample", sample_id, "...\n"))
+  
   rownames(DE_genes_tmp) <- paste0(DE_genes_tmp$sample_id,".",DE_genes_tmp$gene_symbol)
   return(DE_genes_tmp)
 }
