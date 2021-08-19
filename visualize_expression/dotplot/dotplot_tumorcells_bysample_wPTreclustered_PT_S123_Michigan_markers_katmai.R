a061743450b264fec58a703508d55a24882f49a5
Yige Wu <yigewu@macbook-pro-4.local.dhcp.wustl.edu>
2021-08-19 08:21:28 -0500

    dotplot PT S1/2/3 w.PT-A/B/C v1

--- visualize_expression/dotplot/dotplot_tumorcells_bysample_wPTreclustered_PT_S123_Michigan_markers_katmai.R
+++ visualize_expression/dotplot/dotplot_tumorcells_bysample_wPTreclustered_PT_S123_Michigan_markers_katmai.R
@@ -25,7 +25,7 @@ source("./ccRCC_snRNA_analysis/functions.R")
 source("./ccRCC_snRNA_analysis/variables.R")
 library(ggplot2)
 ## set run id
-version_tmp <- 2
+version_tmp <- 1
 run_id <- paste0(format(Sys.Date(), "%Y%m%d") , ".v", version_tmp)
 ## set output directory
 dir_out <- paste0(makeOutDir_katmai(path_this_script), run_id, "/")
