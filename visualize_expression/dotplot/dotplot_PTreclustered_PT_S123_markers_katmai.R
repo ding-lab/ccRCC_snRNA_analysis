c7a94ced6bdc3f3dbf35267214e1a50d2fdbc578
Yige Wu <yigewu@macbook-pro-4.local.dhcp.wustl.edu>
2021-08-30 10:46:07 -0500

    dotplot PT S1/2/3 markers v4

--- visualize_expression/dotplot/dotplot_PTreclustered_PT_S123_markers_katmai.R
+++ visualize_expression/dotplot/dotplot_PTreclustered_PT_S123_markers_katmai.R
@@ -25,7 +25,7 @@ source("./ccRCC_snRNA_analysis/functions.R")
 source("./ccRCC_snRNA_analysis/variables.R")
 library(ggplot2)
 ## set run id
-version_tmp <- 4
+version_tmp <- 5
 run_id <- paste0(format(Sys.Date(), "%Y%m%d") , ".v", version_tmp)
 ## set output directory
 dir_out <- paste0(makeOutDir_katmai(path_this_script), run_id, "/")
@@ -110,12 +110,9 @@ p <- ggplot()
 p <- p + geom_point(data = plotdata_df, mapping = aes(x = features.plot, y = id, color = expvalue_plot, size = pct.exp), shape = 16)
 # p <- p +scale_color_gradient2(midpoint=median(plotdata_df$avg.exp, na.rm = T), low="blue", mid="white",
 #                               high="red", space ="Lab" )
-p <- p + scale_color_gradientn(colours = rev(RColorBrewer::brewer.pal(n = 9, name = "Spectral")[1:5]), guide = guide_colorbar(direction = "horizontal"))
-p <- p + scale_size_continuous(range = c(0, 8), name="% Expressed", guide = guide_legend(direction = "horizontal"))
-# p <- p  + RotatedAxis()
-# p <- p + facet_grid(.~gene_cell_type_group + gene_cell_type1 + gene_cell_type2 + gene_cell_type3 + gene_cell_type4, scales = "free", space = "free", drop = T)
+p <- p + scale_color_gradientn(colours = rev(RColorBrewer::brewer.pal(n = 9, name = "Spectral")[1:5]), guide = guide_colourbar(direction = "horizontal", title = NULL))
+p <- p + scale_size_continuous(breaks = c(25, 50, 75, 100), guide = guide_legend(direction = "horizontal", title = NULL, nrow = 2, byrow = T))
 p <- p + facet_grid(cell_type~gene_cell_type2, scales = "free", space = "free", drop = T)
-
 p <- p + theme(panel.spacing = unit(0, "lines"), panel.grid.major = element_line(colour = "grey80"), 
                panel.border = element_rect(color = "black", fill = NA, size = 0.5),
                panel.background = element_blank())
@@ -123,7 +120,7 @@ p <- p + theme(strip.background = element_rect(color = NA, fill = NA, size = 0.5
                strip.text.x = element_text(angle = 0, vjust = 0.5),
                strip.text.y = element_text(angle = 0, vjust = 0.5),
                axis.text.x = element_text(size = 10, angle=90,hjust=0.95,vjust=0.2))
-p <- p + theme(legend.position = "bottom")
+p <- p + theme(legend.position = "right")
 file2write <- paste0(dir_out, "CellTypeMarkerExp.NotScaled.png")
 png(file = file2write, width = 1200, height = 1000, res = 150)
 print(p)
@@ -148,7 +145,7 @@ p <- p + theme(strip.background = element_rect(color = NA, fill = NA, size = 0.5
                strip.text.x = element_text(angle = 0, vjust = 0.5),
                strip.text.y = element_blank(),
                axis.text.x = element_text(size = 10, angle=90,hjust=0.95,vjust=0.2), axis.title = element_blank())
-p <- p + theme(legend.position = "right", legend.box = "vertical")
+p <- p + theme(legend.position = "right")
 file2write <- paste0(dir_out, "CellTypeMarkerExp.Scaled.png")
 png(file = file2write, width = 1200, height = 1000, res = 150)
 print(p)
