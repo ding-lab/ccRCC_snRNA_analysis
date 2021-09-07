0b76cb89b1499c1c4adc49d7bf949fa23ef9295e
Yige Wu <yigewu@MacBook-Pro-4.local>
2021-09-07 11:50:04 -0500

    avg exp by cell type w. epithelial 35 samples v1

--- annotate_barcode/annotate_barcode_by_tumorcellmanualcluster_PTcluster_othersbysample.R
+++ annotate_barcode/annotate_barcode_by_tumorcellmanualcluster_PTcluster_othersbysample.R
@@ -28,34 +28,31 @@ id_df <- fread(data.table = F, input = "./Resources/Analysis_Results/sample_info
 
 # process PT barcodes -----------------------------------------------------
 ## process PT barcodes
-ptloh_bycluster_df <- pt_loh_df %>%
-  mutate(cell_group = paste0(ifelse(seurat_clusters %in% c(9, 4, 11, 3, 0, 8, 15, 13, 5), "PT_C", "LOH_C"), seurat_clusters))
-barcodes_pt_loh <- pt_bycluster_df$barcode
+pt_bycluster_df <- pt_loh_df %>%
+  filter(seurat_clusters %in% c(9, 4, 11, 3, 0, 8, 15, 13, 5)) %>%
+  mutate(cell_group = paste0("PT_C", seurat_clusters))
+barcodes_pt <- pt_bycluster_df$barcode
 
-# annotate tumor barcodes -------------------------------------------------------
+# annotate other barcodes -------------------------------------------------------
 barcode2tumorsubcluster_df <- barcode2tumorsubcluster_df %>%
   mutate(sample_barcode = paste0(orig.ident, "_", barcode))
 tumor_bycluster_df <- barcode2celltype_df %>%
-  mutate(sample_barcode = paste0(orig.ident, "_", individual_barcode)) %>%
-  filter(sample_barcode %in% barcode2tumorsubcluster_df$sample_barcode)
+  mutate(sample_barcode = paste0(orig.ident, "_", individual_barcode))
 tumor_bycluster_df$cell_group <- mapvalues(x = tumor_bycluster_df$sample_barcode, from = barcode2tumorsubcluster_df$sample_barcode, to = as.vector(barcode2tumorsubcluster_df$Cluster_Name))
-barcodes_tumorcells <- tumor_bycluster_df$sample_barcode
 
-# annotate other barcode --------------------------------------------------
-barcodes_other_df <- barcode2celltype_df %>%
-  mutate(sample_barcode = paste0(orig.ident, "_", individual_barcode)) %>%
-  filter(!(sample_barcode %in% c(barcodes_pt_loh, barcodes_tumorcells))) %>%
-  mutate(cell_group = "other")
+## process non-pt barcodes
+barcode2celltype_df$easyid <- mapvalues(x = barcode2celltype_df$orig.ident, from = id_df$Aliquot.snRNA, to = as.vector(id_df$Aliquot.snRNA.WU))
+nonpt_df <- barcode2celltype_df %>%
+  mutate(barcode = paste0(orig.ident, "_", individual_barcode)) %>%
+  filter(!(barcode %in% barcodes_pt)) %>%
+  mutate(cell_group = paste0(ifelse(Cell_group14_w_transitional == "EMT tumor cells", "EMT tumor cells", Cell_group_w_epithelialcelltypes), "_", easyid))
 ## combine
-cellgroup_df <- rbind(ptloh_bycluster_df %>%
-                        mutate(sample_barcode = barcode) %>%
-                        select(sample_barcode, cell_group),
-                      tumor_bycluster_df %>%
-                        select(sample_barcode, cell_group),
-                      barcodes_other_df %>%
-                        select(sample_barcode, cell_group))
+cellgroup_df <- rbind(pt_bycluster_df %>%
+                        select(barcode, cell_group),
+                      nonpt_df %>%
+                        select(barcode, cell_group))
 table(cellgroup_df$cell_group)
 
 # write output ------------------------------------------------------------
-file2write <- paste0(dir_out, "Barcode_TumorPTLOH_ByCluster.", run_id, ".tsv")
+file2write <- paste0(dir_out, "Barcode_byEpithelialCelltypes_BySample.", run_id, ".tsv")
 write.table(x = cellgroup_df, file = file2write, sep = "\t", row.names = F, quote = F)
