704e898f871a7aa3c756de0c8c8194fbeea2d6a4
Yige Wu <yigewu@macbook-pro-4.local.dhcp.wustl.edu>
2022-02-07 08:54:31 -0600

    featureplot 34 samples merged v1

--- visualize_expression/featureplot/featureplot_selected_genes_on_34merged_katmai.R
+++ visualize_expression/featureplot/featureplot_selected_genes_on_34merged_katmai.R
@@ -1,19 +1,34 @@
 # Yige Wu @WashU Jan 2022
 
 # set up libraries and output directory -----------------------------------
+## getting the path to the current script
+thisFile <- function() {
+  cmdArgs <- commandArgs(trailingOnly = FALSE)
+  needle <- "--file="
+  match <- grep(needle, cmdArgs)
+  if (length(match) > 0) {
+    # Rscript
+    return(normalizePath(sub(needle, "", cmdArgs[match])))
+  } else {
+    # 'source'd via R console
+    return(normalizePath(sys.frames()[[1]]$ofile))
+  }
+}
+path_this_script <- thisFile()
 ## set working directory
-dir_base = "~/Box/Ding_Lab/Projects_Current/RCC/ccRCC_snRNA/"
+dir_base = "/diskmnt/Projects/ccRCC_scratch/ccRCC_snRNA/"
+# dir_base = "~/Box/Ding_Lab/Projects_Current/RCC/ccRCC_snRNA/"
 setwd(dir_base)
+dir_tmp <- getwd()
+print(dir_tmp)
 source("./ccRCC_snRNA_analysis/load_pkgs.R")
 source("./ccRCC_snRNA_analysis/functions.R")
 source("./ccRCC_snRNA_analysis/variables.R")
-source("./ccRCC_snRNA_analysis/plotting.R")
-library(Seurat)
 ## set run id
 version_tmp <- 1
 run_id <- paste0(format(Sys.Date(), "%Y%m%d") , ".v", version_tmp)
 ## set output directory
-dir_out <- paste0(makeOutDir(), run_id, "/")
+dir_out <- paste0(makeOutDir_katmai(path_this_script), run_id, "/")
 dir.create(dir_out)
 
 # input dependencies ------------------------------------------------------
@@ -28,7 +43,7 @@ genes_plot <- c("CA9", "LRP2", "CP", "PCSK6")
 
 # plot by gene ------------------------------------------------------------
 for (gene_plot in genes_plot) {
-  p <- FeaturePlot(object = srat, features = gene_plot, order = T, min.cutoff = "q1", max.cutoff = "q99", cols = c("yellow", "red"))
+  p <- Seurat::FeaturePlot(object = srat, features = gene_plot, order = T, min.cutoff = "q1", max.cutoff = "q99", cols = c("yellow", "red"))
   ## write output
   file2write <- paste0(dir_out, gene_plot, ".png")
   png(file2write, width = 1000, height = 1000, res = 150)
