845b36886891590c6cf0ade278cf0f53f4a6b13c
Yige Wu <yigewu@macbook-pro-4.local.dhcp.wustl.edu>
2022-02-22 16:21:48 -0600

    subsample and recluster v1

--- recluster/recluster_tumorcells/subsample_fixednumber_and_recluster_tumor_cells_in_selected_samples_katmai.R
+++ recluster/recluster_tumorcells/subsample_fixednumber_and_recluster_tumor_cells_in_selected_samples_katmai.R
@@ -22,13 +22,13 @@ setwd(dir_base)
 source("./ccRCC_snRNA_analysis/load_pkgs.R")
 source("./ccRCC_snRNA_analysis/functions.R")
 source("./ccRCC_snRNA_analysis/variables.R")
-source("./ccRCC_snRNA_analysis/plotting.R")
 ## set run id
 version_tmp <- 1
 run_id <- paste0(format(Sys.Date(), "%Y%m%d") , ".v", version_tmp)
 ## set output directory
 dir_out <- paste0(makeOutDir_katmai(path_this_script), run_id, "/")
 dir.create(dir_out)
+library(ggplot2)
 
 # input dependencies ------------------------------------------------------
 ## input seurat object paths
