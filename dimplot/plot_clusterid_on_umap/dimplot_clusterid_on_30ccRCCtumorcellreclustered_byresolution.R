19550feaf2633277212113423f0053c48cc9bfad
yigewu <yigewu@wustl.edu>
2022-04-05 16:21:51 -0500

    findmarkers for all-tumor-cells-reclustered

---
+++ dimplot/plot_clusterid_on_umap/dimplot_clusterid_on_30ccRCCtumorcellreclustered_byresolution.R
@@ -0,0 +1,73 @@
+# Yige Wu @WashU May 2020
+## plot cell type on integration UMAP
+
+# set up libraries and output directory -----------------------------------
+## set working directory
+# dir_base = "~/Box/Ding_Lab/Projects_Current/RCC/ccRCC_snRNA/"
+dir_base = "~/Library/CloudStorage/Box-Box/Ding_Lab/Projects_Current/RCC/ccRCC_snRNA"
+setwd(dir_base)
+packages = c(
+  "rstudioapi",
+  "plyr",
+  "dplyr",
+  "stringr",
+  "reshape2",
+  "data.table",
+  "ggplot2",
+  "ggrastr"
+)
+for (pkg_name_tmp in packages) {
+  library(package = pkg_name_tmp, character.only = T)
+}
+source("./ccRCC_snRNA_analysis/functions.R")
+## set run id
+version_tmp <- 1
+run_id <- paste0(format(Sys.Date(), "%Y%m%d") , ".v", version_tmp)
+## set output directory
+dir_out <- paste0(makeOutDir(), run_id, "/")
+dir.create(dir_out)
+
+# input dependencies ------------------------------------------------------
+## input UMAP info per barcode
+barcode2umap_df <- fread(input = "./Resources/Analysis_Results/integration/seuratintegrate_34_ccRCC_samples/reciprocalPCA_integrate_30_ccRCC_tumorcells/20220404.v1/ccRCC.34Sample.Tumorcells.Integrated.ReciprocalPCA.Metadata.20220404.v1.tsv", data.table = F)
+## input barcode to cluster id
+barcode2cluster_df <- fread(data.table = F, input = "./Resources/Analysis_Results/integration/seuratintegrate_34_ccRCC_samples/FindClusters_30_ccRCC_tumorcells_changeresolutions/20220405.v1/ccRCC.34Sample.Tumorcells.Integrated.ReciprocalPCA.Metadata.ByResolution.20220405.v1.tsv")
+
+# make plot data----------------------------------------------------------
+plot_data_df <- merge(x = barcode2umap_df %>%
+                        select(barcode, UMAP_1, UMAP_2),
+                      y = barcode2cluster_df %>%
+                        select(orig.ident, barcode, integrated_snn_res.0.1, integrated_snn_res.0.2, integrated_snn_res.0.3, integrated_snn_res.0.4, integrated_snn_res.0.5,
+                               integrated_snn_res.1, integrated_snn_res.2),
+                      by = c("barcode"), all.x = T)
+table(plot_data_df$integrated_snn_res.2)
+
+# make plots --------------------------------------------------------------
+for (res_tmp in c(0.1, 0.2, 0.3, 0.4, 0.5, 1, 2)) {
+  plot_data_df[, "cluster_id"] <- factor(plot_data_df[, paste0("integrated_snn_res.", res_tmp)])
+  clusterids <- sort(unique(plot_data_df$cluster_id))
+  colors_bycluster <- Polychrome::palette36.colors(n = (length(clusterids)+1))[-2]
+  names(colors_bycluster) <- clusterids
+  p <- ggplot()
+  p <- p + geom_point_rast(data = plot_data_df, 
+                           mapping = aes(x = UMAP_1, y = UMAP_2, color = cluster_id),
+                           alpha = 1, size = 0.1, shape = 16)
+  p <- p + scale_color_manual(values = colors_bycluster)
+  p <- p + guides(colour = guide_legend(override.aes = list(size=4), title = NULL))
+  p <- p + theme_void()
+  p <- p + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                 panel.background = element_blank(),
+                 axis.line = element_blank())
+  # axis.line = element_line(colour = "black"))
+  p <- p + theme(axis.text.x=element_blank(),
+                 axis.ticks.x=element_blank())
+  p <- p + theme(axis.text.y=element_blank(),
+                 axis.ticks.y=element_blank())
+  p <- p + theme(legend.position="bottom", aspect.ratio=1)
+  ## save as png
+  file2write <- paste0(dir_out, "resolution", res_tmp, ".png")
+  png(filename = file2write, width = 1000, height = 1100, res = 150)
+  print(p)
+  dev.off()
+}
+
---
+++ findmarkers/30ccRCC_tumorcellreclustered/findmarker_30ccRCC_tumorcellreclustered_res0.5_pairwisebycluster.R
@@ -0,0 +1,77 @@
+# Yige Wu @WashU Mar 2022
+## https://satijalab.org/seurat/archive/v3.0/integration.html
+## also used references
+
+# set up libraries and output directory -----------------------------------
+## getting the path to the current script
+thisFile <- function() {
+  cmdArgs <- commandArgs(trailingOnly = FALSE)
+  needle <- "--file="
+  match <- grep(needle, cmdArgs)
+  if (length(match) > 0) {
+    # Rscript
+    return(normalizePath(sub(needle, "", cmdArgs[match])))
+  } else {
+    # 'source'd via R console
+    return(normalizePath(sys.frames()[[1]]$ofile))
+  }
+}
+path_this_script <- thisFile()
+## set working directory
+# dir_base = "~/Library/CloudStorage/Box-Box/Ding_Lab/Projects_Current/RCC/ccRCC_snRNA"
+dir_base = "/diskmnt/Projects/ccRCC_scratch/ccRCC_snRNA/"
+setwd(dir_base)
+packages = c(
+  "rstudioapi",
+  "plyr",
+  "dplyr",
+  "stringr",
+  "reshape2",
+  "data.table",
+  "Seurat",
+  "future",
+  "future.apply"
+)
+
+for (pkg_name_tmp in packages) {
+  library(package = pkg_name_tmp, character.only = T)
+}
+# set up future for parallelization
+plan("multiprocess", workers = 4)
+options(future.globals.maxSize = 10000 * 1024^2)
+## set run id
+version_tmp <- 1
+run_id <- paste0(format(Sys.Date(), "%Y%m%d") , ".v", version_tmp)
+## set output directory
+source("./ccRCC_snRNA_analysis/functions.R")
+dir_out <- paste0(makeOutDir_katmai(path_this_script), run_id, "/")
+dir.create(dir_out)
+
+# input dependencies ------------------------------------------------------
+path_rds <- "/diskmnt/Projects/ccRCC_scratch/ccRCC_snRNA/Resources/Analysis_Results/integration/seuratintegrate_34_ccRCC_samples/reciprocalPCA_integrate_30_ccRCC_tumorcells/20220404.v1/ccRCC.34samples.Tumorcells.SeuratIntegrated.20220404.v1.RDS"
+srat <- readRDS(file = path_rds)
+print("Finish reading the RDS file!\n")
+logfc.threshold.run <- 0.25
+min.pct.run <- 0.1
+min.diff.pct.run <- 0
+
+# process -----------------------------------------------------------------
+Idents(srat) <- "integrated_snn_res.0.5"
+clusters <- unique(Idents(srat))
+pairwise <- combn(clusters, 2)
+
+results_df <- NULL
+for (i in 1:ncol(pairwise)) {
+  markers <- FindMarkers(object = srat, test.use = "wilcox", ident.1 = pairwise[1, i], ident.2 = pairwise[2, i], only.pos = T,
+                         min.pct = min.pct.run, logfc.threshold = logfc.threshold.run, min.diff.pct = min.diff.pct.run, verbose = T)
+  markers$gene_symbol <- rownames(markers)
+  markers$ident.1 <- ident.1 = pairwise[1, i]
+  markers$ident.2 <- ident.1 = pairwise[2, i]
+  results_df <- rbind(results_df, markers)
+}
+
+# save output -------------------------------------------------------------
+file2write <- paste0(dir_out, "tumorcellsreclustered.pairwisebycluster.markers.logfcthreshold.", logfc.threshold.run, ".minpct.", min.pct.run, ".mindiffpct.", min.diff.pct.run, ".tsv")
+write.table(x = results_df, file = file2write, quote = F, sep = F, row.names = F)
+
+
