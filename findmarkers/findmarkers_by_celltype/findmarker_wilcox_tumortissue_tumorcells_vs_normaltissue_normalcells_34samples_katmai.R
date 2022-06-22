f1aea8be14525bde16b26314d705cfd229497c29
yigewu <yigewu@wustl.edu>
2022-06-22 13:27:08 -0500

    findmarkers external data

--- findmarkers/findmarkers_by_celltype/findmarker_wilcox_tumortissue_tumorcells_vs_normaltissue_normalcells_34samples_katmai.R
+++ findmarkers/findmarkers_by_celltype/findmarker_wilcox_tumortissue_tumorcells_vs_normaltissue_normalcells_34samples_katmai.R
@@ -81,8 +81,8 @@ srat@meta.data$Cell_group5 <- mapvalues(x = srat@meta.data$cell_id, from = barco
 srat@meta.data$Cell_group_test <- paste0(srat@meta.data$orig.ident, "_", srat@meta.data$Cell_group5)
 Idents(srat) <- "Cell_group_test"
 table(Idents(srat))
-idents_group1 <- paste0(aliquots_group1, "_", idents_group1)
-idents_group2 <- paste0(aliquots_group2, "_", idents_group2)
+idents_group1 <- paste0(aliquots_group1, "_", idents_group1); idents_group1 <- idents_group1[idents_group1 %in% unique(Idents(srat))]; idents_group1
+idents_group2 <- paste0(aliquots_group2, "_", idents_group2); idents_group2 <- idents_group2[idents_group2 %in% unique(Idents(srat))]; idents_group2
 
 # run findallmarkers ------------------------------------------------------
 sink(file = paste0(dir_out, "log.", run_id, ".txt"))
